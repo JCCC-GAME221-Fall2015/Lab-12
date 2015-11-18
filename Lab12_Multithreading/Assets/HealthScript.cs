1bb2e31a85caa5b0eefdc874470bc67130a1996c
sandrockman <svc-student@RC37815.student.directory.jccc>
2015-11-18 09:59:49 -0600

    OYO

---
+++ Lab12_Multithreading/Assets/HealthScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour {
+
+	public int health = 10;
+	public Text uiText;
+
+	// Use this for initialization
+	void Start () {
+		UpdateUIHealth (0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void UpdateUIHealth(int dmg)
+	{
+		health -= dmg;
+		uiText.text = health.ToString();
+	}
+}
