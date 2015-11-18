81db82f7e5b29f209ef2256997032913269c4cd3
CBroskow <cbroskow@stumail.jccc.edu>
2015-11-18 09:40:51 -0600

    Wednesday morning commit

---
+++ Lab 12/Assets/Scripts/LambdaTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LambdaTest : MonoBehaviour {
+
+	delegate bool isTeenager(Student student);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	bool funcisATeen(Student currStudent)
+	{
+		return (currStudent.age > 12 && currStudent.age < 20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	class Student
+	{
+		public int age;
+
+		public Student()
+		{
+			age = 10;
+		}
+
+		public Student(int age)
+		{
+			this.age =age;
+		}
+	}
+}
