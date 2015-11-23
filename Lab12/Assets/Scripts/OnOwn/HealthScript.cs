ef2baf855043cbfe3a01f124d0ecf04c78eed3c6
Darrick2theend <Darrick2theend@gmail.com>
2015-11-23 00:35:06 -0600

    On Own in progress
    Object movement currently controlled by coroutines; needs to be changed
    to multithreading.

---
+++ Lab12/Assets/Scripts/OnOwn/HealthScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour 
+{
+    [Tooltip("Character Health.")]
+    public int health;
+    [Tooltip("Area to display character health.")]
+    public Text healthDisplay;
+
+    void Start()
+    {
+        healthDisplay.text = health.ToString();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Damager"))
+            Damage();
+    }
+
+    public void Damage()
+    {
+        if (health > 1)
+        {
+            health--;
+            healthDisplay.text = health.ToString();
+        }
+        else
+        {
+            healthDisplay.text = "DEAD";
+        }
+    }
+}
---
+++ Lab12/Assets/Scripts/OnOwn/NonThreadedMoveScript.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NonThreadedMoveScript : MonoBehaviour
+{
+    const int MAX_X = 10;
+    const int MAX_Z = 10;
+
+    bool moving = false;
+
+    void Start()
+    {
+        StartCoroutine(Move());
+    }
+
+    void Update()
+    {
+        if (!moving) { StartCoroutine(Move()); }
+    }
+
+    IEnumerator Move()
+    {
+        float moveTime = 5;
+        MoveType moveType;
+
+        Vector3 start = transform.position;
+        Vector3 target = transform.position;
+        moveType = (MoveType)Random.Range(0, 4);
+
+        //Debug.Log("Start position:  " + start);
+        //Debug.Log("Move type: " + moveType);
+
+        switch (moveType)
+        {
+            case (MoveType.MoveForward):
+                target = target + (Vector3.forward * moveTime);
+                break;
+            case (MoveType.MoveBack):
+                target = target + (Vector3.back * moveTime);
+                break;
+            case (MoveType.MoveRight):
+                target = target + (Vector3.right * moveTime);
+                break;
+            case (MoveType.MoveLeft):
+                target = target + (Vector3.left * moveTime);
+                break;
+        }
+
+        //Debug.Log("Target position: " + target);
+        
+        float elapsedTime = 0;
+        moving = true;
+        //Debug.Log("Moving . . . . . . ");
+
+        while (elapsedTime < moveTime)
+        {
+            transform.position = Vector3.Lerp(start, target, (elapsedTime / moveTime));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+            if (transform.position.x > MAX_X || transform.position.x < -MAX_X || transform.position.z > MAX_Z || transform.position.z < -MAX_Z)
+                CheckBounds();
+        }
+
+        moving = false;
+        //Debug.Log("Movement complete");
+    }
+
+    void CheckBounds()
+    {
+        Vector3 mirrorPos = transform.position;
+
+        if (transform.position.x > MAX_X)
+        {
+            mirrorPos.x = -MAX_X;
+            transform.position = mirrorPos;
+        }
+        if (transform.position.x < -MAX_X)
+        {
+            mirrorPos.x = MAX_X;
+            transform.position = mirrorPos;
+        }
+        if (transform.position.z > MAX_Z)
+        {
+            mirrorPos.z = -MAX_Z;
+            transform.position = mirrorPos;
+        }
+        if (transform.position.z < -MAX_Z)
+        {
+            mirrorPos.z = MAX_Z;
+            transform.position = mirrorPos;
+        }
+    }
+}
