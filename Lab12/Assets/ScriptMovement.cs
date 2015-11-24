ac7a15ea42210fd54e563c638efe3b2593d92aa7
JakeSkov <JakeSkov95@gmail.com>
2015-11-23 23:26:44 -0600

    Final

---
+++ Lab12/Assets/ScriptMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+/// <summary>
+/// @Author Jake Skov
+/// </summary>
+public class ScriptMovement : MonoBehaviour 
+{
+    float time;
+	// Use this for initialization
+	void Start () 
+    {
+        //Lerps Movement
+        Thread lerp = new Thread(() => Vector3.Lerp(transform.position, 
+            new Vector3(Random.Range(-2, 3), Random.Range(-2, 3), 0), 1f * time));
+
+        //Starts thread
+        lerp.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //Updates the time variable
+        time = Time.deltaTime;
+	}
+
+    //Collisions
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Lost")
+        {
+            other.GetComponent<ScriptHealth>().health--;
+        }
+    }
+}
---
+++ Lab12/Assets/ScriptThreading.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class ScriptThreading : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+        //IN LAB CODE
+        Thread worker = new Thread(Go);
+        Debug.Log("Is worker in the Background" + worker.IsBackground);
+        worker.IsBackground = true;
+        Debug.Log("Is worker in the Background" + worker.IsBackground);
+        worker.Start();
+        Go();
+        //END IN LAB CODE
+
+        //LAB REPORT TEST CODE
+        string text = "t1";
+        Thread t1 = new Thread(() => Debug.Log(text));
+        text = "t2";
+        Thread t2 = new Thread(() => Debug.Log(text));
+
+        t1.Start();
+        t2.Start();
+        //END TEST CODE
+	}
+
+    void Go()
+    {
+        Debug.Log("Greetings from " + Thread.CurrentThread.Name);
+    }
+}
